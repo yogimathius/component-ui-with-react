df923207a29840e3ded296493a7140aed8af7c3e
Mathius <info@yogimathius.dev>
2023-07-11 14:39:23 -0700

    chore: add basic react app w storybook

---
+++ components-ui/src/components/Application.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "components/Application.scss";
+
+export default function Application(props) {
+  return (
+    <main className="layout">
+      
+    </main>
+  );
+}
